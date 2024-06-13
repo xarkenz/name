47fa6b1f9160f4aa17f20af2d554833e1b27fb6b
cameron-b63 <cameron.bond63@gmail.com>
2024-06-13 01:09:02 -0500

    define default ELF header values (some wrong)

--- name-as/src/elf_utils.rs
+++ name-as/src/elf_utils.rs
@@ -1,11 +1,90 @@
 // Utilities to assemble to ELF.
 // This file contains relevant structs and methods to produce valid ET_REL and ET_EXEC files.
 
-// This struct was derived from information found at https://en.wikipedia.org/wiki/Executable_and_Linkable_Format
+use std::collections::HashMap;              // Used to define modes by name
+
+// The following data definitions are required to construct ELF files.
+// The meaning of each field is detailed in the appropriate struct.
+
+// ELF File Header fields
+
+// All of the following pertain to the e_ident field, and can be edited granularly below.
+const EI_MAG: [u8; 4] = [0x7F, b'E', b'L', b'F'];  // ELF magic bytes to signal that an ELF file follows
+const EI_CLASS: u8 = 1;         // Specify 32-bit format
+const EI_DATA: u8 = 2;          // Specify BIG endian
+const EI_VERSION: u8 = 1;       // Set original version for first iteration of constructing relocatable
+const EI_OSABI: u8 = 0;         // Specify System V IBA (specified in original ELF TIS)
+const EI_ABIVERSION: u8 = 0;    // Not needed
+const EI_PAD: [u8; 7] = [0, 0, 0, 0, 0, 0, 0];  // Built-in padding
+
+// This is the full e_ident field (a complete constant. should never need to be changed).
+const e_ident_default: [u8; 16] = [EI_MAG[0], EI_MAG[1], EI_MAG[2], EI_MAG[3], EI_CLASS, EI_DATA, EI_VERSION, EI_OSABI, EI_ABIVERSION, EI_PAD];
+
+
+// I am defining all feasible ET modes for later.
+const ET_NONE: u16 = 0;
+const ET_REL: u16 = 1;
+const ET_EXEC: u16 = 2;
+const ET_DYN: u16 = 3;
+// All ELFs will first be constructed with e_type set to ET_REL. The linker handles any changes.
+const e_type_default: u16 = ET_REL;
+
+// The e_machine field value 8 represents the MIPS instruction set.
+const e_machine_default: u16 = 8;
+
+// the e_entry address cannot be known, as it corresponds to the global main function.
+// the linker will need to handle filling in this field.
+const e_entry_default: u32 = 0;
+
+// the e_phoff field can be known ahead of time, since the program header follows the ELF header by convention.
+// it is simply the size of the elf header in bytes, as is already specified to be 52 for a 32-bit executable.
+const e_phoff_default: u32 = 52;
+
+// by convention, the section header follows all sections and any other headers. its value cannot be known ahead of time.
+// thus, the e_shoff field must be filled in after the full module has been assembled.
+const e_shoff_default: u32 = 0;
+
+// for MIPS, the MIPS ELF specification lays out the e_flags field as follows (irrelevant fields commented out but retained for completeness):
+const EF_MIPS_NONREORDER: u32 = 0x00000001;         // No reordering of code to be done by assembler (better for education)
+// const EF_MIPS_PIC: u32 = 0x00000002;             // Contains position-independent code
+// const EF_MIPS_CPIC: u32 = 0x00000004;            // The PIC present follows standard conventions for calling PIC
+// const EF_MIPS_UCODE: u32 = 0x00000010;           // This file contains UCODE (obsolete)
+const EF_MIPS_ABI2: u32 = 0x00000020;               // This file has an EI_CLASS of ELFCLASS32 (ours does)
+// const EF_MIPS_OPTIONS_FIRST: u32 = 0x00000080;   // The .MIPS.options section in this file contains descriptors for ld. (not us)
+// const EF_MIPS_ARCH_ASE: u32 = 0x0f000000;        // Architectural extensions below are present:
+// const EF_MIPS_ARCH_ASE_MDMX: u32 = 0x08000000;   // Uses MDMX multimedia extensions
+// const EF_MIPS_ARCH_ASE_M16: u32 = 0x04000000;    // Uses MIPS-16 ISA extensions (we implement some of these as pseudo-instructions)
+const EF_MIPS_ARCH: u32 = 0x30000000;               // Architecture of the code (mips version) version IV is implmented in NAME
+
+// The bitwise-or combination of selected flags gives the proper e_flags.
+const e_flags_default: u32 = EF_MIPS_ARCH | EF_MIPS_ABI2 | EF_MIPS_NONREORDER;
+
+// As stated, the ELF header size is known to be 52 for 32-bit binaries.
+const e_ehsize_default: u16 = 52;
+
+// Similarly, the program header entry size is known to be 32 for 32-bit binaries. It's from the way the struct is defined.
+const e_phentsize_default: u16 = 32;
+
+// For our use case, the number of entries in the program header is known.
+// Each object file we assemble prior to linking will have 1 entry for .text, 1 entry for .data, and 1 entry for .bss (heap).
+const e_phnum_default: u16 = 3;
+
+// Just like the other sizes, e_shentsize is known because it's derived from the struct.
+const e_shentsize_default: u16 = 40;
+
+// For our use case, e_shnum is known.
+// Each object file we assemble needs all program header entries, along with 1 entry for .note (debug/lineinfo).
+const e_shnum_default:u16 = 4;
+
+// By convention, e_shstrndx is set to the last value in the section header. Therefore,
+const e_shstrndx_default:u16 = e_shnum_default;
+
+// This struct for the ELF file header was derived from information found at https://en.wikipedia.org/wiki/Executable_and_Linkable_Format
+// as well as information from the original TIS ELF specification.
 #[repr(C)]                                  // Used to avoid aligment issues. Not sure it's necessary but honestly better safe than sorry in this case.
 #[derive(Debug, Default, Clone, Copy)]
 struct Elf32Header{
-    e_ident: [u8; 10],      // Contains { {EI_MAG0 .. EIMAG3}, EI_CLASS, EI_DATA, EI_VERSION, EI_OSABI, EI_ABIVERSION, EI_PAD}
+    e_ident: [u8; 16],      // Contains { {EI_MAG0 .. EIMAG3}, EI_CLASS, EI_DATA, EI_VERSION, EI_OSABI, EI_ABIVERSION, EI_PAD}
     e_type: u16,            // Identifies object file type (ET_REL before linking, ET_EXEC after)
     e_machine: u16,         // Target ISA
     e_version: u32,         // ELF version (will be 1 prior to linking, and incremented to 2 once linking is complete)
@@ -53,9 +132,14 @@ struct Elf32SectionHeader{
     sh_entsize: u32,        // Size in bytes of each entry for sections that contain fixed-size entries (think tables)
 }
 
+
+fn create_new_elf_file_header() -> Result<Elf32Header, &'static str> {
+    
+}
+
 /*
 sh_flags:
-p
+
 0x1 = SHF_WRITE (writable)
 0x2 = SHF_ALLOC (occupies memory during construction)
 0x4 = SHF_EXECINSTR (executable)
