ced044087e58c83da53db7eefa857826f6069e1b
cameron-b63 <cameron.bond63@gmail.com>
2024-11-18 01:56:08 -0600

    Completed implementation of relocate_symtab_links (untested!)

--- name-ld/src/relocate.rs
+++ name-ld/src/relocate.rs
@@ -8,24 +8,24 @@ use crate::relocate_helpers::{check_duplicate_symbols, relocate_symtab_links, re
 pub fn relocate(sections: Vec<Vec<u8>>, offsets: Vec<Vec<u32>>) -> Result<Elf, String> {
     // Relocation is the process of putting things where they ought to go.
     // The first thing to do is create a mutable ELF (it will still be relocatable). Now that the sections have been consolidated, we just need to operate on the structured data.
-    let mut new_elf = create_new_elf(sections, ElfType::Relocatable);
+    let new_elf: Elf = create_new_elf(sections, ElfType::Relocatable);
     
     // Now that the ELF is mutable, the first thing to do is fix the offsets for the .symtab links into .strtab.
     // Each symbol needs to have the appropriate offset added to fix the indexing.
     // This has been extracted to a function.
-    let _ = relocate_symtab_links(&mut new_elf, &offsets);
+    let relocated_symtab: Elf = relocate_symtab_links(new_elf, &offsets);
 
     // Next, duplicate global symbols need to be checked for, as well as duplicate, same-scope local symbols.
     // This has also been extracted to a function.
-    let _ = check_duplicate_symbols(&new_elf)?;
+    let checked_symbols: Elf = check_duplicate_symbols(relocated_symtab, &offsets)?;
 
     // Now, each entry in .rel needs to be reconciled.
     // This is a complex process, so it's also been extracted to a function.
-    let _ = relocate_text_entries(&mut new_elf, &offsets);
+    let relocated = relocate_text_entries(checked_symbols, &offsets);
 
     // Now that each entry in .rel has been reconciled, and the symtab adjusted appropriately, there's nothing left to do.
     // Extract the sections from the new ELF, and create an executable out of them.
-    let new_sections: Vec<Vec<u8>> = new_elf.sections.iter().enumerate().filter_map(|(idx, section)| match idx {
+    let new_sections: Vec<Vec<u8>> = relocated.sections.iter().enumerate().filter_map(|(idx, section)| match idx {
         0 | 2 => None,
         _ => Some(section.clone()),
     }).collect();
--- name-ld/src/relocate_helpers.rs
+++ name-ld/src/relocate_helpers.rs
@@ -1,15 +1,48 @@
 // This file contains the helpers for the relocate function
 
-use name_core::elf_def::Elf;
+use name_core::{elf_def::{Elf, ElfType}, elf_utils::{create_new_elf, parse_elf_symbols}};
 
-pub fn relocate_symtab_links(_elf: &mut Elf, _offsets: &Vec<Vec<u32>>) -> () {
+/// This function is responsible for adjusting the symtab link indices. 
+/// It's entirely based on the .strtab offsets.
+pub fn relocate_symtab_links(elf: Elf, offsets: &Vec<Vec<u32>>) -> Elf {
+    let symtab_offsets: Vec<u32> = offsets[4].clone();
+    let mut current_offset: u32 = 0;
+    let mut current_offset_idx: usize = 0;
+    let mut previous_st_name: u32 = 0;
+
+    // For each entry in the symbol table, if the symbol has a smaller index than the previous, or it's equal (at 0), it needs to add the offset.
+    // IMPORTANT (POSSIBLE EDGE CASE MUST CHECK LATER): I think this can crash at runtime with some really weird files. 
+    // Note the check for offset range. It doesn't seem watertight.
+    let symbol_table = parse_elf_symbols(&elf.sections[4]);
+    let new_symbol_table: Vec<u8> = symbol_table.iter().map(|symbol| {
+        let mut new_symbol = symbol.clone();
+        if symbol.st_name <= previous_st_name {
+            // ^ DEBUG POINT IS THIS
+            current_offset += symtab_offsets[current_offset_idx + 1];
+            current_offset_idx += 1;
+        }
+
+        previous_st_name = symbol.st_name;
+        new_symbol.st_name = current_offset + symbol.st_name;
+
+        new_symbol.to_bytes()
+    }).flatten().collect();
+
+    // Make a new ELF with the new .symtab
+    let new_sections: Vec<Vec<u8>> = elf.sections.iter().enumerate().map(|(idx, section)| match idx {
+        4 => new_symbol_table.clone(),
+        _ => section.clone(),
+    }).collect();
+
+    return create_new_elf(new_sections, ElfType::Relocatable);
+}
+
+/// This function checks first for duplicate global symbols, 
+/// then it checks for duplicate local symbols in the same local space (based on calculated offsets).
+pub fn check_duplicate_symbols(_elf: Elf, _offsets: &Vec<Vec<u32>>) -> Result<Elf, String> {
     todo!();
 }
 
-pub fn check_duplicate_symbols(_elf: &Elf) -> Result<(), String> {
-    todo!();
-}
-
-pub fn relocate_text_entries(_elf: &mut Elf, _offsets: &Vec<Vec<u32>>) -> () {
+pub fn relocate_text_entries(_elf: Elf, _offsets: &Vec<Vec<u32>>) -> Elf {
     todo!();
 }
